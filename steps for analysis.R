25b3b9c11cbfe748d79bf810453d51a0a33e7c21
Phil Yates <66975068+philipayates@users.noreply.github.com>
2023-11-28 17:28:57 -0600

    Update steps for analysis.R

--- steps for analysis.R
+++ steps for analysis.R
@@ -7,7 +7,7 @@ y <- peak$Peak_Flow
 
 # Run the robustlikelihood estimation
 t <- (1:131)/131
-congareeRLL <- RLLgev(t,y,0.35,50000)
+congareeRLL <- RLLgev(t,y,0.3,50000)
 
 # Save the parameter estimates (for data visualization purposes)
 congaree.est <- as.data.frame(congareeRLL$parm.est) %>%
@@ -16,7 +16,7 @@ congaree.est <- as.data.frame(congareeRLL$parm.est) %>%
 # 1%-chance flood standard error estimation
 Q99.SE <- NULL
 for(i in 1:131){
-  Q99.SE[i] <- RLLqsd(congareeRLL$parm.est[i,1],y,congareeRLL$wts[,i],0.35,50000,
+  Q99.SE[i] <- RLLqsd(congareeRLL$parm.est[i,1],y,congareeRLL$wts[i,],0.35,50000,
                       congareeRLL$parm.est[i,2:5])
 }
 
